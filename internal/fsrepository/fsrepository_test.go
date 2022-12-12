271faa73c6ff169047b38de56eb7d57184b18720
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2022-12-12 14:00:40 +1100

    test: use temporary directory instead of local .testdata

--- internal/fsrepository/fsrepository_test.go
+++ internal/fsrepository/fsrepository_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFSRepository(t *testing.T) {
+func TestFSRepository_ListGoSourceFiles(t *testing.T) {
 	t.Parallel()
 
 	t.Run("panics when given root does not exist", func(t *testing.T) {
@@ -100,18 +100,18 @@ func TestFSRepository(t *testing.T) {
 
 	t.Run("relative root", func(t *testing.T) {
 		t.Parallel()
-		assert.NoError(t, os.MkdirAll(".testdata/a/b", 0o700))
-		defer func() { assert.NoError(t, os.RemoveAll(".testdata")) }()
+		dir := t.TempDir()
+		assert.NoError(t, os.MkdirAll(dir+"/a/b", 0o700))
 
-		assert.NoError(t, os.WriteFile(".testdata/readme.md", []byte("read me"), 0o600))
-		assert.NoError(t, os.WriteFile(".testdata/source1.go", []byte("source data 1"), 0o600))
-		assert.NoError(t, os.WriteFile(".testdata/source1_test.go", []byte("test data 1"), 0o600))
-		assert.NoError(t, os.WriteFile(".testdata/a/source2.go", []byte("source data 2"), 0o600))
-		assert.NoError(t, os.WriteFile(".testdata/a/source2_test.go", []byte("test data 2"), 0o600))
-		assert.NoError(t, os.WriteFile(".testdata/a/b/source3.go", []byte("source data 3"), 0o600))
-		assert.NoError(t, os.WriteFile(".testdata/a/b/source3_test.go", []byte("test data 3"), 0o600))
+		assert.NoError(t, os.WriteFile(dir+"/readme.md", []byte("read me"), 0o600))
+		assert.NoError(t, os.WriteFile(dir+"/source1.go", []byte("source data 1"), 0o600))
+		assert.NoError(t, os.WriteFile(dir+"/source1_test.go", []byte("test data 1"), 0o600))
+		assert.NoError(t, os.WriteFile(dir+"/a/source2.go", []byte("source data 2"), 0o600))
+		assert.NoError(t, os.WriteFile(dir+"/a/source2_test.go", []byte("test data 2"), 0o600))
+		assert.NoError(t, os.WriteFile(dir+"/a/b/source3.go", []byte("source data 3"), 0o600))
+		assert.NoError(t, os.WriteFile(dir+"/a/b/source3_test.go", []byte("test data 3"), 0o600))
 
-		repository := fsrepository.New(".testdata")
+		repository := fsrepository.New(dir)
 		files := repository.ListGoSourceFiles()
 		assert.Equal(t, []*gosourcefile.GoSourceFile{
 			gosourcefile.New("a/b/source3.go", []byte("source data 3")),
