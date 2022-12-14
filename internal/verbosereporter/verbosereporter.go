d8d4a8281e8d1b4474e90e0471f98744bd46fd1c
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2022-12-15 00:56:23 +1100

    feat: add verbosereporter implementation

---
+++ internal/verbosereporter/verbosereporter.go
@@ -0,0 +1,29 @@
+package verbosereporter
+
+import (
+	"github.com/gtramontina/ooze/internal/ooze"
+	"github.com/gtramontina/ooze/internal/result"
+)
+
+type VerboseReporter struct {
+	logger   ooze.Logger
+	delegate ooze.Reporter
+}
+
+func New(logger ooze.Logger, delegate ooze.Reporter) *VerboseReporter {
+	return &VerboseReporter{
+		logger:   logger,
+		delegate: delegate,
+	}
+}
+
+func (r *VerboseReporter) AddDiagnostic(diagnostic result.Result[string]) {
+	r.logger.Logf("registering diagnostic…")
+	r.delegate.AddDiagnostic(diagnostic)
+}
+
+func (r *VerboseReporter) Summarize() *ooze.ReportSummary {
+	r.logger.Logf("summarizing report…")
+
+	return r.delegate.Summarize()
+}
---
+++ internal/verbosereporter/verbosereporter_test.go
@@ -0,0 +1,45 @@
+package verbosereporter_test
+
+import (
+	"testing"
+
+	"github.com/gtramontina/ooze/internal/basicreporter"
+	"github.com/gtramontina/ooze/internal/oozetesting/fakelogger"
+	"github.com/gtramontina/ooze/internal/result"
+	"github.com/gtramontina/ooze/internal/verbosereporter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVerboseReporter(t *testing.T) {
+	t.Parallel()
+
+	t.Run("logs when adding a diagnostic", func(t *testing.T) {
+		t.Parallel()
+
+		logger := fakelogger.New()
+
+		verbosereporter.New(
+			logger,
+			basicreporter.New(),
+		).AddDiagnostic(result.Ok("dummy"))
+
+		assert.Equal(t, []string{
+			"registering diagnostic…",
+		}, logger.LoggedLines())
+	})
+
+	t.Run("logs when summarizing", func(t *testing.T) {
+		t.Parallel()
+
+		logger := fakelogger.New()
+
+		verbosereporter.New(
+			logger,
+			basicreporter.New(),
+		).Summarize()
+
+		assert.Equal(t, []string{
+			"summarizing report…",
+		}, logger.LoggedLines())
+	})
+}
